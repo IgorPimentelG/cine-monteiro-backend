bb941f9f13e98faaf7b351187de7b6fa356bbfa6
ezzequiias <86009508+ezzequiias@users.noreply.github.com>
2021-11-16 21:04:54 -0300

    Mockito test service class

---
+++ src/test/java/com/cine/monteiro/junit/mockito/ClienteServiceMockitoTest.java
@@ -0,0 +1,67 @@
+package com.cine.monteiro.junit.mockito;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.repository.config.RepositoryBeanDefinitionParser;
+import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.cine.monteiro.domain.model.user.User;
+import com.cine.monteiro.domain.repository.UserRepository;
+import com.cine.monteiro.domain.services.UserService;
+import com.cine.monteiro.exception.UserException;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+
+public class ClienteServiceMockitoTest {
+
+	@Autowired
+	private UserService service;
+	
+	@MockBean
+	private UserRepository repository;
+	
+	@Test
+	public void getUsersTest() throws UserException {
+		when(repository.findAll()).thenReturn(Stream.of(new User("12388400430", "Tadeu", "99885564", LocalDate.now(), "testes@gmail.com", "teste123", "ROLE_USER"),
+				new User("12388400420", "Marcelo", "99662354", LocalDate.now(), "testes2@gmail.com", "teste345", "ROLE_USER")).collect(Collectors.toList()));
+			assertEquals(2, service.listar().size());
+	}
+	
+	@Test
+	public void getUsersByEmail() {
+		String email = "mockito123@gmail.com";
+		when(repository.findByEmail(email)).thenReturn((User) Stream.of(new User("12388400430", "Tadeu", "99885564", LocalDate.now(), "mockito123@gmail.com", "teste123", "ROLE_USER")).collect(Collectors.toList()));
+	
+		assertEquals("mockito123@gmail.com", repository.findByEmail(email));
+	}
+	
+	@Test
+	public void saveUserTest() throws UserException {
+		User user = new User("12388400430", "Tadeu", "99885564", LocalDate.now(), "mockito123@gmail.com", "teste123", "ROLE_USER");
+		when(repository.save(user)).thenReturn(user);
+		assertEquals(user, service.salvar(user));
+	}
+	
+	@Test
+	public void deleteUserTest() {
+		User user = new User("12388400430", "Tadeu", "99885564", LocalDate.now(), "mockito123@gmail.com", "teste123", "ROLE_USER");
+		repository.delete(user);
+		
+		verify(repository, times(1)).delete(user);
+	}
+}
