bb8e76d47b1e31f50450fb1895378fb3b625b2e8
Igor Pimentel <igor.pimentel.msi@hotmail.com>
2021-11-30 21:02:27 -0300

    Adicionando testes de segurança

--- src/test/java/com/cine/monteiro/integracao/SecutiryConfigTest.java
+++ src/test/java/com/cine/monteiro/integracao/SecutiryConfigTest.java
@@ -1,5 +1,17 @@
 package com.cine.monteiro.integracao;
 
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringJUnitWebConfig
 public class SecutiryConfigTest {
+	
+	// cases usuários
+	
+	// cases admin
 
 }
